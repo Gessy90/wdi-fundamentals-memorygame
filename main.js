31fb991b03abc4c12b30214f891721c60f4b694a
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2018-11-14 14:37:12 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 
+console.log("Up and running!");
+var cardOne =
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+ var cardTwo =
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
+var cardThree =
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+var cardFour =
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   };
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
